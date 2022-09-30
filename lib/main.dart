c89dcda062e8a9d45d7b26a45c426650bdca33ee
abdallah <01276613517a@gmail.com>
2022-09-30 09:18:42 +0200

    add show dialog

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:q4k/audio_player.dart';
 import 'package:q4k/constants.dart';
 import 'package:q4k/main_screen.dart';
 import 'firebase_options.dart';
+import 'hadith_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,12 +14,41 @@ void main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // Intialize Firevase app
   Future<FirebaseApp> _initializeFirebase() async {
     FirebaseApp firebaseApp = await Firebase.initializeApp();
     return firebaseApp;
   }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //     WidgetsBinding.instance.addPostFrameCallback((_) async {
+  //       await showDialog(
+  //         context: context,
+  //         builder: (BuildContext context) => Center(
+  //           child: AlertDialog(
+  //             insetPadding: const EdgeInsets.all(20),
+  //             contentPadding: const EdgeInsets.all(0),
+  //             content: SizedBox(
+  //               height: 200,
+  //               width: MediaQuery.of(context).size.width,
+  //               child: const Center(
+  //                 child: Text("Alert dialog in app start up"),
+  //               ),
+  //             ),
+  //             actions: const <Widget>[],
+  //           ),
+  //         ),
+  //       );
+  //     });
+  //   }
 
   // This widget is the root of your application.
   @override
@@ -35,7 +65,7 @@ class MyApp extends StatelessWidget {
           future: _initializeFirebase(),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.done) {
-              return MainScreen();
+              return const MainScreen();
             }
             return const Center(
               child: CircularProgressIndicator(),
