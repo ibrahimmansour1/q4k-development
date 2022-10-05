957149d280b550839fab740452bcd89959edeee6
abdallah <01276613517a@gmail.com>
2022-10-05 19:09:50 +0200

    not accredited

--- lib/screens/sign_in/cubit/cubit.dart
+++ lib/screens/sign_in/cubit/cubit.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 import 'states.dart';
 
@@ -8,19 +9,42 @@ class SocialLoginCubit extends Cubit<SocialLoginStates> {
 
   static SocialLoginCubit get(context) => BlocProvider.of(context);
 
+ Map<String,dynamic>? _userData;
   void userLogin({
     required String email,
     required String password,
   }) {
     emit(SocialLoginLoadingState());
-    FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: email,
-        password: password
-    ).then((value){
+    FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email, password: password)
+        .then((value) {
       print(value.user!.email);
       emit(SocialLoginSuccessState(value.user!.uid));
-    }).catchError((error){
+    }).catchError((error) {
       emit(SocialLoginErrorState(error.toString()));
     });
   }
+
+  Future<UserCredential> signInWithFacebook() async {
+
+    final LoginResult result = await FacebookAuth.instance.login(permissions:['email']);
+
+
+    if (result.status == LoginStatus.success) {
+
+      final userData = await FacebookAuth.instance.getUserData();
+
+      _userData = userData;
+    } else {
+      print(result.message);
+    }
+
+     // welcome = _userData?['email'];
+
+
+    final OAuthCredential facebookAuthCredential = FacebookAuthProvider.credential(result.accessToken!.token);
+
+    return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+  }
+
 }
